3a94e399427a2708a8a82ef0f9ad9f2a2c22ad43
1000marin <leexjin@gmail.com>
2022-06-10 15:46:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/prj3/MainActivity11.java
@@ -0,0 +1,16 @@
+package com.example.prj3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main11);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/prj3/MainActivity8.java
@@ -0,0 +1,28 @@
+package com.example.prj3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main8);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // 시간 차를 두고 실행하고 싶은 함수 호출
+                Intent intent = new Intent(getApplicationContext(), MainActivity9.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+
+            }
+        }, 2000); // 3000 대신 시간차를 두기 원하는 만큼의 수를 써주세요 ( 단위 : 밀리초 )
+    }
+}
